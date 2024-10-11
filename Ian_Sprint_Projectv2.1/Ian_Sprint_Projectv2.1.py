ea9b2222b0edc455c410cecbd46867bfee661f5f
IanSu18 <144109031+IanSu18@users.noreply.github.com>
2024-10-11 06:05:06 +0000

    added the convergent of kg to pound

--- Ian_Sprint_Projectv2.1/Ian_Sprint_Projectv2.1.py
+++ Ian_Sprint_Projectv2.1/Ian_Sprint_Projectv2.1.py
@@ -1,1 +1,8 @@
-#&
+def kg_pound():
+    kg = float(input('Please enter the weight in kilograms: '))
+    pound = kg/0.45359237
+    print(pound, 'pounds')
+def pound_kg():
+    pound = float(input('Please entr the weight in pound: '))
+    kg = pound*0.45359237
+    print(kg, 'kg')
