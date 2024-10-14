873740dae84337b244674f9a87839fda8078e38f
Russellouo <rchien25_student@ivycollegiate.org>
2024-10-14 05:44:38 +0000

    adds funfacts of weights

--- Russell_Sprint_Projectv2.1/Russell_Sprint_Projectv2.1.py
+++ Russell_Sprint_Projectv2.1/Russell_Sprint_Projectv2.1.py
@@ -1,1 +1,5 @@
-##rip russell
+weight_facts = [
+    "A person or thing on the moon may be weightless, but have the same mass everywhere ",
+    "Any two masses have a gravitational attraction for one another, and the greater the mass, the greater the pull ",
+    "In 1795, a kilogram was equivalent to a liter (or a cubic decimeter) of water "
+]
