af185eec4ef94894832c3355c36e8bcafe852b20
IanSu18 <144109031+IanSu18@users.noreply.github.com>
2024-10-14 05:46:07 +0000

    adds exceptions and printing random funfacts.

--- Ian_Sprint_Projectv2.1/Ian_Sprint_Projectv2.1.py
+++ Ian_Sprint_Projectv2.1/Ian_Sprint_Projectv2.1.py
@@ -1,8 +1,15 @@
 def kg_pound():
-    kg = float(input('Please enter the weight in kilograms: '))
-    pound = kg/0.45359237
-    print(pound, 'pounds')
+    try:
+        kg = float(input('Please enter the weight in kilograms: '))
+        pound = kg/0.45359237
+        print(pound, 'pounds')
+    except ValueError:
+        print("Try again. Please enter a valuable number.")
+        print()
 def pound_kg():
-    pound = float(input('Please entr the weight in pound: '))
-    kg = pound*0.45359237
-    print(kg, 'kg')
+    try:
+        pound = float(input('Please entr the weight in pound: '))
+        kg = pound*0.45359237
+        print(kg, 'kg')
+    except ValueError: 
+        print("Try again. Please enter a valuable number.")
